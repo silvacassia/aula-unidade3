443147a8a442133b62fc5044c0df93d2e1cddffa
Cassia Silva <111803968+silvacassia@users.noreply.github.com>
2026-03-11 20:35:53 -0300

    Add files via upload

---
+++ Ler.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ler {
+    public static void main(String[] args) {
+        // Scanner Class
+        int numero;
+        Scanner leitor = new Scanner(System.in);
+
+        numero = leitor.nextInt();
+        System.out.println("O número é: " + numero);
+    }
+}
